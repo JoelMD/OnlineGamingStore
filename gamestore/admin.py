648988de50b6f76257ffc8677a382c637e68c127
JoelMD <77885579+JoelMD@users.noreply.github.com>
2022-04-07 09:26:20 +0400

    Add files via upload

---
+++ gamestore/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+
+admin.site.register(Console)
+admin.site.register(PC)
+admin.site.register(Seller)
+admin.site.register(Game)
+admin.site.register(Customer)
+admin.site.register(Item)
+admin.site.register(Order)
+admin.site.register(OrderThing)
+admin.site.register(Device)
